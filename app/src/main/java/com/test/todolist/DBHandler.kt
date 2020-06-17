f745261efa4cc58f51647cd174a4bb956beaae98
Mukul Kumar <mukul.2121@gmail.com>
2020-06-17 20:52:51 +0530

    added recycler view and connected with database

---
+++ app/src/main/java/com/test/todolist/DBHandler.kt
@@ -0,0 +1,56 @@
+package com.test.todolist
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import androidx.core.content.contentValuesOf
+import com.test.todolist.DTO.ToDo
+import java.util.ArrayList
+
+class DBHandler(val context : Context) : SQLiteOpenHelper(context, DB_NAME, null, DB_VERSION){
+    override fun onCreate(db: SQLiteDatabase) {
+        val createToDoTable = "  CREATE TABLE $TABLE_TODO (" +
+                "$COL_ID integer PRIMARY KEY AUTOINCREMENT," +
+                "$COL_CREATED_AT datetime DEFAULT CURRENT_TIMESTAMP," +
+                "$COL_NAME varchar);"
+        val createToDoItemTable =
+            "CREATE TABLE $TABLE_TODO_ITEM (" +
+                    "$COL_ID integer PRIMARY KEY AUTOINCREMENT," +
+                    "$COL_CREATED_AT datetime DEFAULT CURRENT_TIMESTAMP," +
+                    "$COL_TODO_ID integer," +
+                    "$COL_ITEM_NAME varchar," +
+                    "$COL_IS_COLPLETED integer);"
+
+        db.execSQL(createToDoTable)
+        db.execSQL(createToDoItemTable)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+
+    fun addToDo(toDo : ToDo) : Boolean{
+        val db = writableDatabase
+        val cv = ContentValues()
+        cv.put(COL_NAME, toDo.name)
+        val result = db.insert(TABLE_TODO, null, cv)
+        return result != (-1).toLong()
+    }
+
+    //Reading the data
+    fun getToDos() : MutableList<ToDo>{
+        val result : MutableList<ToDo> = ArrayList()
+        val db = readableDatabase
+        val queryResult = db.rawQuery("SELECT * FROM $TABLE_TODO", null)
+        if(queryResult.moveToFirst()){
+            do {
+                val todo = ToDo()
+                todo.id= queryResult.getLong(queryResult.getColumnIndex(COL_ID))
+                todo.name = queryResult.getString(queryResult.getColumnIndex(COL_NAME))
+                result.add(todo)
+            }while (queryResult.moveToNext())
+        }
+        queryResult.close()
+        return result
+    }
+}
